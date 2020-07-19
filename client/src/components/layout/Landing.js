50e6e2611434aab2f63e306a4b6bce4a7d82bd23
Mina Mina <minakhamesi@gmail.com>
2020-07-19 11:16:30 -0400

    change landing

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -12,7 +12,7 @@ const Landing = ({isAuthenticated}) => {
         <div className="dark-overlay">
             <div className="landing-inner">
                 <h1 className="x-large">Developer Connector</h1>
-                <p className="lead">Create developer profile/portfolio, share posts and get help from other developers</p>
+                <p className="lead">Create developer profile, share posts, join/create groups and get help from other developers</p>
                 <div className="buttons">
                     <Link to="/register" className="btn btn-primary">Sign Up</Link>
                     <Link to="/login" className="btn">Login</Link>
